a0fd0e353b9ced74c3ed4babdfc7a0bcbebe79b1
Evan Wang <evan.wang@boardware.com>
2023-07-13 14:46:42 +0800

    feat: 全局異常處理

--- example/lib/app/app_launcher.dart
+++ example/lib/app/app_launcher.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:common/common/top.dart';
+import 'package:example/app_base/app_base.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'launcher/strategy/base_sample_launcher_strategy.dart';
@@ -10,9 +14,34 @@ class AppLauncher {
   static void launch(
     BaseSampleLauncherStrategy launcherStrategy,
   ) {
-    runApp(MyApp(
-      launcherStrategy: launcherStrategy,
-      route: RouteConfig(),
-    ));
+    // 将启动策略中带入的配置写进环境变量BuildConfig给全局使用
+    BuildConfig.envName = launcherStrategy.envName;
+    BuildConfig.host = launcherStrategy.host;
+    BuildConfig.isDebug = launcherStrategy.isDebug;
+    BuildConfig.proxy = launcherStrategy.proxy;
+    BuildConfig.proxyPort = launcherStrategy.proxyPort;
+
+    void reportErrorAndLog(FlutterErrorDetails details) {
+      final errorMsg = {
+        "exception": details.exceptionAsString(),
+        "stackTrace": details.stack.toString(),
+      };
+      logE('reportErrorAndLog: $errorMsg');
+      showToast('reportErrorAndLog: ${errorMsg['exception']}');
+    }
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      //获取 widget build 过程中出现的异常错误
+      reportErrorAndLog(details);
+    };
+
+    runZonedGuarded(
+        () => runApp(MyApp(
+              launcherStrategy: launcherStrategy,
+              route: RouteConfig(),
+            )), (error, stack) {
+      // 没被我们catch的异常
+      reportErrorAndLog(FlutterErrorDetails(stack: stack, exception: error));
+    });
   }
 }
