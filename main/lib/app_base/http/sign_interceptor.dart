7b0a58defa06cd46efce72fa4fc260a2fd6871b7
Akashiwen <343611864@qq.com>
2023-10-30 23:17:18 +0800

    fix: 修正token應該存放的位置

--- main/lib/app_base/http/sign_interceptor.dart
+++ main/lib/app_base/http/sign_interceptor.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart' as dio;
-import 'package:get/get_connect/http/src/multipart/form_data.dart';
+import 'package:main/app_base/config/build_config.dart';
 
 /// 请求头拦截器
 class SignInterceptor extends dio.Interceptor {
   @override
   void onRequest(
       dio.RequestOptions options, dio.RequestInterceptorHandler handler) {
+    options.headers['x-token'] = BuildConfig.token;
     super.onRequest(options, handler);
   }
 }
